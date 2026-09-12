971d44d98ba13b6cb7bf6e2d15c62ab915623c88
flnapol <flnapol@users.noreply.github.com>
2026-09-12 02:42:18 +0200

    Aggiunge widget Meteo Flaskio 3x3 con orologio

---
+++ app/src/main/java/it/flaskio/meteo/WeatherWidgetClock.kt
@@ -0,0 +1,20 @@
+package it.flaskio.meteo
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+
+class WeatherWidgetClock : AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        manager: AppWidgetManager,
+        ids: IntArray
+    ) {
+        WidgetUpdater.update(
+            context,
+            manager,
+            ids,
+            R.layout.widget_clock
+        )
+    }
+}
