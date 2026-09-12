a98e1359562c40d120c4006dbf2bdeafe7337b47
flnapol <flnapol@users.noreply.github.com>
2026-09-12 02:12:41 +0200

    Fix Java 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
 }
 android {
+    compileOptions { sourceCompatibility = JavaVersion.VERSION_17; targetCompatibility = JavaVersion.VERSION_17 }
     namespace = "it.flaskio.meteo"
     compileSdk = 35
     defaultConfig {
